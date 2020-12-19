a2b9604fa6117cb526a3067c52d0c6332a3bb1c8
CentOsDogE <XxBoomer@users.noreply.github.com>
2020-12-19 21:28:22 +0700

    Create Test.lua

---
+++ AllStarTD/Combo afk/Test.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/XxBoomer/script/main/AllStarTD/Combo%20afk/AntiAFK.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/XxBoomer/script/main/AllStarTD/Combo%20afk/Teleport.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/XxBoomer/script/main/AllStarTD/Combo%20afk/TimerGUI.lua'))()
