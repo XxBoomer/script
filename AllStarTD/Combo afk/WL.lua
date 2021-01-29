c9f1e0545d7f5060f48b63de495126972e521abf
CentOsDogE <XxBoomer@users.noreply.github.com>
2021-01-29 12:34:48 +0700

    Update WL.lua

--- AllStarTD/Combo afk/WL.lua
+++ AllStarTD/Combo afk/WL.lua
@@ -1,5 +1,5 @@
 repeat wait() until game:IsLoaded()
-if game.Players.LocalPlayer.UserId == 712512766 or game.Players.LocalPlayer.UserId == 2059423968 or game.Players.LocalPlayer.UserId == 1354897157 then
+if game.Players.LocalPlayer.UserId == 712512766 or game.Players.LocalPlayer.UserId == 2059423968 or game.Players.LocalPlayer.UserId == 1354897157 or game.Players.LocalPlayer.UserId == 1871947727 then
 else game.Players.LocalPlayer:Kick("Share cái con cac")
     wait(2) game:Shutdown()
     end
