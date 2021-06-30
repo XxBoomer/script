9d74bbf623992f988a9ed8944d92f274c286261e
CentOsDogE <XxBoomer@users.noreply.github.com>
2021-07-01 02:42:07 +0700

    Create main.lua

---
+++ ad/main.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XxBoomer/script/main/ad/teleport.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XxBoomer/script/main/ad/timegui.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XxBoomer/script/main/ad/antiafk.lua"))();
