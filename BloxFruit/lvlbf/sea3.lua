ea56303b3db9fd10e6d05c2098e99d920363fd67
CentOsDogE <XxBoomer@users.noreply.github.com>
2021-12-11 20:35:34 +0700

    Create sea3.lua

---
+++ BloxFruit/lvlbf/sea3.lua
@@ -0,0 +1,4 @@
+lvlold = game:GetService("Players").LocalPlayer.Data.Level.Value
+lvlnew = lvlold+300
+repeat wait() until game:GetService("Players").LocalPlayer.Data.Level.Value >= lvlnew or game:GetService("Players").LocalPlayer.Data.Level.Value >= 2100
+game.Players.LocalPlayer:Kick("đã cày xong\nusername: "..game.Players.LocalPlayer.Name.."\nLvl Old: "..lvlold.."\nlvl stop: "..tostring(lvlnew))
