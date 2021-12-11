7fb33d8c0ca0dd7bfde77eedb50404568170388d
CentOsDogE <XxBoomer@users.noreply.github.com>
2021-12-11 21:00:04 +0700

    Create scanlvl.lua

---
+++ BloxFruit/lvlbf/scanlvl.lua
@@ -0,0 +1,11 @@
+repeat wait() until game:IsLoaded()
+
+if game.PlaceId == 2753915549 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/XxBoomer/script/main/BloxFruit/lvlbf/sea1.lua"))()
+elseif game.PlaceId == 4442272183 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/XxBoomer/script/main/BloxFruit/lvlbf/sea2.lua"))()
+elseif game.PlaceId == 7449423635 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/XxBoomer/script/main/BloxFruit/lvlbf/sea3.lua"))()
+else
+    print("")
+end
