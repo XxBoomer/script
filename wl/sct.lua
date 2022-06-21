6666d4c0d5bb43f4ce220c9cb9466c8b350d5823
CentOsDogE <XxBoomer@users.noreply.github.com>
2022-06-21 23:12:40 +0700

    Update sct.lua

--- wl/sct.lua
+++ wl/sct.lua
@@ -1,7 +1,7 @@
 repeat wait() until game:IsLoaded()
-ip = loadstring(game:HttpGet("https://raw.githubusercontent.com/XxBoomer/script/main/wl/lstip.lua"))()
+ip = game:HttpGet("https://raw.githubusercontent.com/XxBoomer/script/main/wl/lstip.lua")
 ipcheck = game:HttpGet("https://v4.ident.me")
-if table.find(ip,ipcheck) then 
+if string.find(ip,ipcheck) then 
     setfpscap(40)
 else 
     game.Players.LocalPlayer:Kick("Wrong Ip Address")
