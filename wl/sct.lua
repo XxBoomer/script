cab4ee2e1dd5609024994a3deba8936c6d74136f
CentOsDogE <XxBoomer@users.noreply.github.com>
2022-07-12 18:20:08 +0700

    Update sct.lua

--- wl/sct.lua
+++ wl/sct.lua
@@ -2,7 +2,7 @@ repeat wait() until game:IsLoaded()
 ip = game:HttpGet("https://raw.githubusercontent.com/XxBoomer/script/main/wl/lstip.lua")
 ipcheck = game:HttpGet("https://v4.ident.me")
 if string.find(ip,ipcheck) then 
-    setfpscap(29)
+    setfpscap(30)
 else 
     game.Players.LocalPlayer:Kick("\nNo Found Your IP: "..ipcheck.."\nContact Script Creator In ClipBoard")
     setclipboard("!BomUwU#6367")
