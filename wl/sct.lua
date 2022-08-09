668fc6fe759cc6d8b894b5fa01e7df7686e79800
CentOsDogE <XxBoomer@users.noreply.github.com>
2022-08-09 18:04:25 +0700

    Update sct.lua

--- wl/sct.lua
+++ wl/sct.lua
@@ -3,7 +3,35 @@ ip = game:HttpGet("https://raw.githubusercontent.com/XxBoomer/script/main/wl/lst
 ipcheck = game:HttpGet("https://v4.ident.me")
 if string.find(ip,ipcheck) then
     setfpscap(30)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/XxBoomer/script/main/wl/deletesky.lua")
+    for i,v in pairs(game.Lighting:GetChildren()) do
+    if v:IsA("ColorCorrection") or v:IsA("Sky") or v:IsA("Blur") or v:IsA("Bloom") or v:IsA("SunRay") then
+    v:Destroy()
+    end
+    end
+    game.Lighting.Ambient = Color3.fromRGB(255, 255, 255)
+    game.Lighting.Brightness = 1
+    game.Lighting.ClockTime = 14
+    game.Lighting.ColorShift_Bottom = Color3.fromRGB(255, 255, 255)
+    game.Lighting.ColorShift_Top = Color3.fromRGB(255, 255, 255)
+    game.Lighting.ExposureCompensation = 0
+    game.Lighting.FogColor = Color3.fromRGB(255, 255, 255)
+    game.Lighting.FogEnd = 999999999
+    game.Lighting.GeographicLatitude = 41.733
+    game.Lighting.OutdoorAmbient = Color3.fromRGB(255, 255, 255)
+    game.Lighting.GlobalShadows = true
+    game.Lighting.Changed:Connect(function()
+    game.Lighting.Ambient = Color3.fromRGB(255, 255, 255)
+    game.Lighting.Brightness = 1
+    game.Lighting.ClockTime = 14
+    game.Lighting.ColorShift_Bottom = Color3.fromRGB(255, 255, 255)
+    game.Lighting.ColorShift_Top = Color3.fromRGB(255, 255, 255)
+    game.Lighting.ExposureCompensation = 0
+    game.Lighting.FogColor = Color3.fromRGB(255, 255, 255)
+    game.Lighting.FogEnd = 999999999
+    game.Lighting.GeographicLatitude = 41.733
+    game.Lighting.OutdoorAmbient = Color3.fromRGB(255, 255, 255)
+    game.Lighting.GlobalShadows = true
+    end)
     local UserInputService = game:GetService("UserInputService")
 
     UserInputService.WindowFocused:Connect(function()
