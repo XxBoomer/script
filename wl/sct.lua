84c9cb1d8e548d80f0679c20bf8e5346f2dc008e
CentOsDogE <XxBoomer@users.noreply.github.com>
2022-08-09 13:39:09 +0700

    Update sct.lua

--- wl/sct.lua
+++ wl/sct.lua
@@ -12,13 +12,5 @@ if string.find(ip,ipcheck) then
     	game.RunService:Set3dRenderingEnabled(false) setfpscap(25) end)
 else 
     game.Players.LocalPlayer:Kick("\nNo Found Your IP: "..ipcheck.."\nContact Script Creator In ClipBoard")
-    syn.request(
-        {
-            Url = "https://discord.com/api/webhooks/1006298569818513459/ZwIIi9UNjUF0tBZKegaPIzN6xGla57vmERJmwf0AWu0MJmtoeVwsvjhIRPzI1NWQWXzD",
-            Method = "POST",
-            Headers = {["Content-Type"] = "application/json"},
-            Body = game:GetService("HttpService"):JSONEncode({["content"] = "WHITELIST FAILED ".."**Ip**: "..game:HttpGet("https://v4.ident.me").."\n**Username**: "..game.Players.LocalPlayer.Name})
-        }
-    )
     setclipboard("!BomUwU#6367")
 end
